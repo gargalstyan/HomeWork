94fe081bef71d25a2beb666ff6797803e54a5232
garikdavit galstyan <garikgalstyan8@gmail.com>
2023-09-19 19:58:49 +0400

    Some changes

---
+++ Factorial/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter n");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(Factorial(n));
+static double Factorial(int n)
+{
+    if (n <= 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter n");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(Fibonacci(n));
+static double Fibonacci(int n)
+{
+    if (n <= 0)
+    {
+        return 0;
+    }
+    else if (n == 1) 
+    { 
+        return 1;
+    }
+    else
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+}
+
